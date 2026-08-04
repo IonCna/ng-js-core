160aa56d4fd4797344bc81354c52e80591f7f1ab
Flores Rodriguez, Max Alejandro - Externo <MaxAlejandro.FloresRodriguez@fuller.com.mx>
2026-08-04 10:39:44 -0600

    disable viewChild temp

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,8 +1,5 @@
 import angular from "angular";
-import { decorateController } from "@/core/controller-decorator"
-
 export type { EmbeddedViewRef } from "@/core/refs"
-export { viewChild, type ViewChildOptions, type QuerySignal } from "@/core/viewChild"
 
 export const CoreModule = angular.module("ng.core", [])
-CoreModule.decorator("$controller", decorateController)
+//CoreModule.decorator("$controller", decorateController)
